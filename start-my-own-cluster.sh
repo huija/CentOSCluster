e4531ce67298a4f0cb5a031ef729b96a498c48f8
huija <1150555483@qq.com>
2019-04-17 22:18:42 +0800

    hbase-2.0.5_HA

--- start-my-own-cluster.sh
+++ start-my-own-cluster.sh
@@ -3,3 +3,4 @@ start-dfs.sh
 start-yarn.sh
 mr-jobhistory-daemon.sh start historyserver
 read -p "run [yarn-daemon.sh start resourcemanager] on your standby please!!" var
+start-hbase.sh
--- stop-my-own-cluster.sh
+++ stop-my-own-cluster.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+stop-hbase.sh
 read -p "run [yarn-daemon.sh stop resourcemanager] on your standby please!!" var
 mr-jobhistory-daemon.sh stop historyserver
 stop-yarn.sh
